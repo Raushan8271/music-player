630b3ee333c72810e27002ca271569015ea01c84
Raushan8271 <raushan8271@gmail.com>
2021-09-04 13:53:28 +0530

    Add Music

--- js/index.js
+++ js/index.js
@@ -5,8 +5,6 @@ let current = document.getElementById("current")
 let progress = document.getElementById("progress")
 
 let isMusicPlay = false
-let st = 0
-let w = 1
 
 function play() {
     isMusicPlay = !isMusicPlay
@@ -17,7 +15,7 @@ function play() {
 music.addEventListener("timeupdate", (e) => {
     // console.log(e)
     let { currentTime, duration } = e.srcElement;
-    totalTime.innerHTML = `Duration: ${Math.floor(duration)} Sec`;
+    totalTime.innerHTML = `Current: ${Math.floor(duration)} Sec`;
     current.innerHTML = `Duration: ${Math.floor(currentTime)} Sec`;
     let avgTime = (currentTime / duration) * 100
     progress.style.width = `${avgTime}%`
