62c50fd7c5331d5dbb998bcbeaee8dc3ee685fdc
Raushan8271 <raushan8271@gmail.com>
2021-09-04 19:08:40 +0530

    add music

--- js/index.js
+++ js/index.js
@@ -3,6 +3,9 @@ let btn = document.getElementById("btn")
 let totalTime = document.getElementById("duration")
 let current = document.getElementById("current")
 let progress = document.getElementById("progress")
+let canvasProgress = document.getElementById("canvasProgress")
+let progress_div = document.getElementById("progress_div")
+
 
 let isMusicPlay = false
 
@@ -13,10 +16,21 @@ function play() {
 }
 
 music.addEventListener("timeupdate", (e) => {
-    // console.log(e)
     let { currentTime, duration } = e.srcElement;
     current.innerHTML = `Current: ${Math.floor(currentTime)} Sec`;
     totalTime.innerHTML = `Duration: ${Math.floor(duration)} Sec`;
     let avgTime = (currentTime / duration) * 100
     progress.style.width = `${avgTime}%`
+    
+})
+
+progress_div.addEventListener('click', (e) => {
+    let { offsetX } = e
+    let { clientWidth } = e.srcElement
+    // console.log(offsetX)
+    console.log(clientWidth)
+    let avgTime = (offsetX / clientWidth) * 100
+    // console.log(avgTime)
+    // console.log(music.currentTime)
+    // music.currentTime = avgTime
 })
